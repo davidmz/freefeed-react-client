47649a5c4cdbc8bc394944440a60ffdab89c5a97
David Mzareulyan <david@hiero.ru>
2016-06-09 13:31:17 +0300

    Wider preview for YouTube, Vimeo and Google Docs

--- src/components/iframely.jsx
+++ src/components/iframely.jsx
@@ -42,16 +42,21 @@ export default class Iframely  extends React.Component {
   }
 
   render() {
-    const className = classnames({
+    const classNames = {
       'link-preview': true,
       'show-all': this.state.showAll,
       'expanded': this.state.expanded,
-    });
+    };
+
+    const knownLinkType = this.knownLinkType(this.props.url);
+    if (knownLinkType !== null) {
+      classNames[knownLinkType] = true;
+    }
 
     return (
       <div className="link-preview-container">
-        <div ref="wrapper" className={className}>
-          <div ref="prv" dangerouslySetInnerHTML={this.getHTML()} />
+        <div ref="wrapper" className={classnames(classNames)}>
+          <div ref="prv" className="link-preview-inner" dangerouslySetInnerHTML={this.getHTML()} />
         </div>
         {this.state.showAll ? false : (
           <div className="link-preview-expand">
@@ -66,4 +71,43 @@ export default class Iframely  extends React.Component {
       </div>
     );
   }
+
+  knownLinkType = (url) => {
+    const types = [
+      {
+        type: 'google-docs',
+        re: [
+          /https:\/\/(?:docs|drive)\.google\.com\/(forms|document|presentation|spreadsheets|file)\/d\//i
+        ]
+      },
+      {
+        type: 'youtube',
+        re: [
+          /^https?:\/\/(?:www\.)?youtube\.com\/(?:tv#\/)?watch\/?\?(?:[^&]+&)*v=([a-zA-Z0-9_-]+)/i,
+          /^https?:\/\/youtu.be\/([a-zA-Z0-9_-]+)/i,
+          /^https?:\/\/m\.youtube\.com\/#\/watch\?(?:[^&]+&)*v=([a-zA-Z0-9_-]+)/i,
+          /^https?:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9_-]+)/i,
+          /^https?:\/\/www\.youtube\.com\/v\/([a-zA-Z0-9_-]+)/i,
+          /^https?:\/\/www\.youtube\.com\/user\/[a-zA-Z0-9_-]+\/?\?v=([a-zA-Z0-9_-]+)/i,
+          /^https?:\/\/www\.youtube-nocookie\.com\/v\/([a-zA-Z0-9_-]+)/i          
+        ]
+      },
+      {
+        type: 'vimeo',
+        re: [
+          /^https?:\/\/(?:www\.)?vimeo\.com\/(\d+)/i
+        ]
+      }
+    ];
+
+    for (let i = 0; i < types.length; i++) {
+      const {type, re} = types[i];
+      if (re.some(r => r.test(url))) {
+        return type;
+      }
+    }
+
+    return null;
+  }
+
 }
